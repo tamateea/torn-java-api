e7615d9c3b006a603af128c2bdb560e5cd3c7e5b
tamatea <tamateea@gmail.com>
2021-06-28 06:02:38 +1200

    Selection class will be extended with functionality for different selections

---
+++ lib/src/main/java/com/tamatea/model/selection/Selection.java
@@ -0,0 +1,8 @@
+package com.tamatea.model.selection;
+
+import com.tamatea.model.entity.Entity;
+
+public abstract class Selection<T extends Entity> {
+
+
+}
---
+++ lib/src/main/java/com/tamatea/model/selection/impl/player/Ammo.java
@@ -0,0 +1,10 @@
+package com.tamatea.model.selection.impl.player;
+
+import com.tamatea.model.entity.impl.user.User;
+import com.tamatea.model.selection.Selection;
+
+public class Ammo extends Selection<User> {
+
+
+
+}
