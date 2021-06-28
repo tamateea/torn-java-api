c670d329676ed08e5c64f900e0fc12b053c670e0
tamatea <tamateea@gmail.com>
2021-06-28 14:23:35 +1200

    ammo selection and related models

---
+++ lib/src/main/java/com/tamatea/model/entity/objects/AmmoSize.java
@@ -0,0 +1,4 @@
+package com.tamatea.model.entity.objects;
+
+public enum AmmoSize {
+}
--- lib/src/main/java/com/tamatea/model/selection/impl/player/Ammo.java
+++ lib/src/main/java/com/tamatea/model/selection/impl/player/Ammo.java
@@ -1,10 +1,22 @@
 package com.tamatea.model.selection.impl.player;
 
 import com.tamatea.model.entity.impl.user.User;
+import com.tamatea.model.entity.objects.AmmoSize;
+import com.tamatea.model.entity.objects.AmmoType;
 import com.tamatea.model.selection.Selection;
 
+//these are retrieved as an array Ammo[]
 public class Ammo extends Selection<User> {
 
+    int ammoID;//todo why is this camelCase when the rest of the api is snake_case?
+    int typeID;//todo why is this camelCase when the rest of the api is snake_case?
+    AmmoSize size;
+    AmmoType type;
+    int quantity;
+    boolean equipped;
 
-
+    @Override
+    public String selectionName() {
+        return "ammo";
+    }
 }
