20699693f81f0677a92bcd86f7b36fe45a66467e
tamatea <tamateea@gmail.com>
2021-06-28 14:24:25 +1200

    commented user status model suggested changes

--- lib/src/main/java/com/tamatea/model/entity/impl/user/Status.java
+++ lib/src/main/java/com/tamatea/model/entity/impl/user/Status.java
@@ -2,10 +2,10 @@ package com.tamatea.model.entity.impl.user;
 
 public class Status {
 
-    String desciption;
+    String description;//This probably shoul also be an enum.
     String details;
-    String state;
-    String color;
+    String state;//this should be an enum.
+    String color;//this could be an enum.
     int until;
 
 }
