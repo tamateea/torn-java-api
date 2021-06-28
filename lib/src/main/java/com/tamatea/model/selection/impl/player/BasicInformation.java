a6770c730979ccc53bd636ebb35953c9e944c991
tamatea <tamateea@gmail.com>
2021-06-28 14:22:32 +1200

    Basic information selection

---
+++ lib/src/main/java/com/tamatea/model/selection/impl/player/BasicInformation.java
@@ -0,0 +1,14 @@
+package com.tamatea.model.selection.impl.player;
+
+import com.tamatea.model.entity.impl.user.Status;
+import com.tamatea.model.entity.objects.Gender;
+
+public class BasicInformation {
+
+    int level;
+    Gender gender;
+    int player_id;
+    String name;
+    Status status;
+
+}
